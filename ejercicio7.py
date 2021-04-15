7a3372417beffc55786a289c46f09b1a6cafbdbb
feehx25 <49326819+feehx25@users.noreply.github.com>
2021-04-14 21:59:50 -0300

    segui el 7

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,4 +1,20 @@
 
+dias = {
+        "1":31,
+        "2":28,
+        "3":31,
+        "4":30,
+        "5":31,
+        "6":30,
+        "7":31,
+        "8":31,
+        "9":30,
+        "10":31,
+        "11":30,
+        "12":31
+    }
+
+
 def bisiesto(año):
     if año % 4 == 0 and año % 100 == 0:
         if año % 400 == 0:
@@ -8,32 +24,82 @@ def bisiesto(año):
     elif año % 4 == 0:
         return "Es bisiesto"
 
-
-
 def mesdia(mes):
-    from datetime import date
-    from calendar import monthrange
-    
-    cantidad_de_dias= monthrange(2011, mes)[1]
-    return cantidad_de_dias
+    if bisiesto(año) == "Es bisiesto":
+        dias["2"] = 29
+    return dias[str(mes)]
+
+
+def valid(dia,mes,año):
+    if dia > 0:
+        if 1 < mes and mes < 12:
+            if dia > mesdia(mes):
+                return "Fecha no valida"
+            else:
+                return "Es valido"
+        else:
+            return "Fecha no valida"
+    else:
+        return "Fecha no valida"
+
+def faltan(dia,mes,año):
+    if valid(dia,mes,año) == "Fecha no valida":
+        return valid(dia,mes,año)
+    elif valid(dia,mes,año) == "Es valido":
+        fal= mesdia(mes) - (dia)
+        return fal
+
+
+def findeaño(dia,mes,año):
+    if valid(dia,mes,año) == "Es valido":
+        mesRestan = 12 - mes
+        r=0
+        if mesRestan != 0:
+            for i in (range(1,mesRestan + 1)):
+                a = mesdia(i)  
+                r = r + a  
+        fal = r + faltan(dia,mes,año)
+        return fal
+    else:
+        return "Fecha no valida"
 
 
 
-if funcion == 1:
 
+func= int(input("""Que funcion desea realizar
+1.Bisiesto
+2.Dias del mes
+3.Fecha valida
+4.Cuantos dias faltan para que termine el mes
+5.Cuantos dias faltan para que termine el año
+"""))
+
+if func == 1:
     año = int(input("ingrese el año "))
     print(bisiesto(año))
 
-elif funcion == 2:
+elif func == 2:
     mes = int(input("""Ingrese el mes del cual se quieren saber los dias
     1.enero / 2.febrero / 3.marzo / 4.abril / 5.mayo / 6.junio
     7.julio / 8.agosto / 9.septiembre / 10.octubre / 11.noviembre
     12.diciembre
     """))
+    año= int(input("Ingrese el año "))
     print(f"Tiene {mesdia(mes)} Dias")
-    
-    
-    funcion= int(input("""Que funcion desea realizar
-1.bisiesto
-2.dias del mes
-"""))
+
+elif func == 3: 
+    dia= int(input("Ingrese el dia "))
+    mes= int(input("Ingrese el mes "))
+    año= int(input("Ingrese el año "))
+    print(valid(dia,mes,año))
+
+elif func == 4: 
+    dia= int(input("Ingrese el dia "))
+    mes= int(input("Ingrese el mes "))
+    año= int(input("Ingrese el año "))
+    print(faltan(dia,mes,año))
+elif func == 5: 
+    dia= int(input("Ingrese el dia "))
+    mes= int(input("Ingrese el mes "))
+    año= int(input("Ingrese el año "))
+    print(findeaño(dia,mes,año))
