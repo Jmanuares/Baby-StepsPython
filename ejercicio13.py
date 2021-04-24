318780f36abd662fb7883da2390078f9bc977487
jmanuares <jmanuares@gmail.com>
2021-04-24 11:44:36 -0300

    añadi el 13

---
+++ ejercicio13.py
@@ -0,0 +1,16 @@
+""" . Escribir un programa que compare dos strings y devuelva True si el primero es la versión capitalizada del segundo.
+
+Nota: Por ejemplo, la versión capitalizada de la palabra “programar” es “PROGRAMAR”. """
+
+
+palabra = input("ingrese una palabra: ")
+palabra2 = input("ingrese otra palabra: ")
+def capital(str1,str2):
+    if palabra == palabra2.upper():
+        return True
+    else: 
+        return False
+if capital(palabra,palabra2):
+    print(f"La palabra {palabra} es la version capitalizada de {palabra2}")
+else:
+    print(f"La palabra {palabra} no es la version capitalizada de palabra {palabra2}")
