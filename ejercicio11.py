eb5bf87cb3b5abe73fabd72c5e917f14232f4c7d
jmanuares <jmanuares@gmail.com>
2021-04-24 11:04:06 -0300

    añadi el 11

---
+++ ejercicio11.py
@@ -0,0 +1,31 @@
+""" Escribir una función que dada la cantidad de ejercicios de un examen, 
+y el porcentaje necesario de ejercicios bien resueltos necesario para aprobar dicho examen,
+revise un grupo de exámenes. Para ello, en cada paso debe preguntar la cantidad de ejercicios resueltos
+por el alumno, indicando con un valor centinela que no hay más exámenes a revisar.
+Debe mostrar por pantalla el porcentaje correspondiente a la cantidad de ejercicios resueltos 
+respecto a la cantidad de ejercicios del examen y una leyenda que indique si aprobó o no. """
+import helper
+def aprobo(n):
+    if n > nota or n == nota:
+        return 1
+    else:
+        return 0   
+
+
+while True:
+    try:
+        examen = int(input("ingrese la cantidad de ejercicios que tiene el examen: "))
+        nota = int(input("¿Con que porcentaje aprobara? "))
+        ejercicios = int(input("¿Cuantos ejercicios resolvio correctamente? "))
+        porcentaje = (100/examen)*ejercicios
+        if aprobo(porcentaje) == 1:
+            print(f"El alumno aprobo con un {porcentaje}%")
+            break
+        else:
+            print(f"El alumno desaprobo con un {porcentaje}%")
+            break
+    except ValueError:
+        print("porfavor ingrese valores numericos")
+        continue
+
+
